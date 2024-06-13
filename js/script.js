5ddb5e5d35ea99f33749145df23fbc56df3efa9f
Pavel Butkariov <butkarevpavel@gmail.com>
2024-06-13 22:03:58 +0300

    career:mobile version with plagin swiper-slider complited

---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // const slider1 = new Swiper('.slider-1', {
+
+  // });
+
+  // const slider2 = new Swiper('.slider-2', {
+
+  // });
+
+  const slider3 = new Swiper('.career-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+
+  });
+});
