5344b5b345d9194aebc2fa7d2bd1a455de6f2caa
Pavel <136801313+GitPaul-dev@users.noreply.github.com>
2024-06-16 23:29:30 +0300

    mobile version is done
    * header:add mobile version without nav-bar
    
    * hero:add mobile version, btn is not worrking yet
    
    * about:add mobile version
    
    * vacancies:add tab-buttons
    
    * vacancies:mobile version is done
    
    * advantages:mobile version is done
    
    * footer:mobile version is done
    
    * team:mobile version copleted
    
    * career:mobile version with plagin swiper-slider completed
    
    * products:add mobile version with plugin swiper-slider
    
    * header:nav-bar stylization
    
    * pop-up-form:stylization with submit script draft
    
    * add adaptive to 991px wide
    
    * mobile hover-effects completed
    
    * add form validation script
    
    * add typography custom properties

---
+++ js/script.js
@@ -0,0 +1,123 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  const productsSliders = document.querySelectorAll('.products-slider');
+
+  productsSliders.forEach((item) => {
+    let slider = new Swiper(item, {
+      navigation: {
+        nextEl: item.querySelector('.swiper-button-next'),
+        prevEl: item.querySelector('.swiper-button-prev'),
+      },
+
+      pagination: {
+        el: item.querySelector('.swiper-pagination'),
+        type: 'bullets',
+        clickable: 'true',
+      },
+
+      effect: 'coverflow',
+      coverflowEffect: {
+        rotate: 0,
+        slideShadows: true,
+      },
+
+      breakpoints: {
+        480: {
+          spaceBetween: 40,
+        },
+
+        740: {
+          spaceBetween: 50,
+        },
+      },
+
+      initialSlide: 2,
+      slidesPerView: 2,
+      spaceBetween: 30,
+      loop: true,
+      centeredSlides: true,
+    });
+  });
+
+  const slider2 = new Swiper('.career-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+      },
+    }
+  });
+
+  // Валидация формы и переключение на окно подтверждения отправки формы
+  const popUpCloseBtn = document.getElementById('pop-up-close')
+  const popupForm = document.getElementById('pop-up-form');
+  const formSendDiv = document.getElementById('form-send');
+  const formSendCloseBtn = document.getElementById('form-send-close');
+  const nameInput = document.getElementById('name-input');
+  const phoneInput = document.getElementById('phone-input');
+  const nameErr = document.getElementById('name-error');
+  const phoneErr = document.getElementById('phone-error');
+
+  function isValidName(name) {
+    const pattern = /^[а-яА-Яa-zA-Z]+$/;
+    return pattern.test(name);
+  }
+
+  function isValidPhone(phone) {
+    const pattern = /^\d+$/;
+    return pattern.test(phone);
+  }
+
+  popupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (!nameInput.value && !phoneInput.value) {
+      nameErr.innerHTML = '*value';
+      phoneErr.innerHTML = '*value';
+      return;
+    }
+
+    if (!nameInput.value) {
+      nameErr.innerHTML = '*value';
+      return;
+    }
+
+    if (!phoneInput.value) {
+      phoneErr.innerHTML = '*value';
+      return;
+    }
+
+    if (!isValidName(nameInput.value)) {
+      nameErr.innerHTML = '*value';
+      return;
+    }
+
+    if (!isValidPhone(phoneInput.value)) {
+      phoneErr.innerHTML = '*value';
+      return;
+    }
+
+    popupForm.classList.add('invisible');
+    formSendDiv.classList.remove('invisible');
+
+    popupForm.reset();
+  });
+
+  function classToggle(firstEl, secondEl, clickedBtn, className) {
+    clickedBtn.addEventListener('click', () => {
+      firstEl.classList.add(className);
+      secondEl.classList.remove(className);
+    });
+  }
+
+  classToggle(formSendDiv, popupForm, popUpCloseBtn, 'invisible');
+  classToggle(formSendDiv, popupForm, formSendCloseBtn, 'invisible');
+});
